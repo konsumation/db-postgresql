b7d0d143c0709f5e77e2846e82103774ae3caf28
k0nsti <kkarpman@clickforit.de>
2024-03-13 20:32:39 +0100

    fix: move some columns

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,13 +1,14 @@
 create table category (
-    id SERIAL, name VARCHAR(30) not null PRIMARY KEY, description VARCHAR(80), unit VARCHAR(10), fractionalDigits INT
+    id SERIAL, name VARCHAR(30) not null PRIMARY KEY, description VARCHAR(80)
 );
 
 COMMENT ON COLUMN category.unit IS 'physical unit like kWh or m3';
 
 COMMENT ON COLUMN category.fractionaldigits IS 'display precission';
 
+-- FOREIGN key on category name and id columns instead of two different
 CREATE TABLE meter (
-    id SERIAL, name VARCHAR(30) not null PRIMARY KEY, category VARCHAR(30) not null REFERENCES category(name), description VARCHAR(80), unit VARCHAR(10), fractionalDigits INT
+    id SERIAL, meterserial varchar(100), categoryid int not null REFERENCES category(id), category VARCHAR(30) not null REFERENCES category(name), description VARCHAR(80), unit VARCHAR(10), fractionalDigits INT
 );
 
 COMMENT ON COLUMN meter.unit IS 'physical unit like kWh or m3';
@@ -16,6 +17,6 @@ COMMENT ON COLUMN meter.fractionaldigits IS 'display precission';
 
 create table values (
     value decimal not null,
-    category VARCHAR(30) not NULL references category(name),
+    meter int not NULL references meter(id),
     date timestamp not null
 );
