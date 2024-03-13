ff1f3cd9d0176c840843be6765793011a7c81dcc
Markus Felten <Markus.Felten@gmx.de>
2024-03-13 21:33:56 +0100

    style: reformat

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,5 +1,7 @@
-create table category (
-    id SERIAL, name VARCHAR(30) not null PRIMARY KEY, description VARCHAR(80)
+CREATE TABLE category (
+    id SERIAL,
+    name VARCHAR(30) not null PRIMARY KEY,
+    description VARCHAR(80)
 );
 
 COMMENT ON COLUMN category.unit IS 'physical unit like kWh or m3';
@@ -8,15 +10,23 @@ COMMENT ON COLUMN category.fractionaldigits IS 'display precission';
 
 -- FOREIGN key on category name and id columns instead of two different
 CREATE TABLE meter (
-    id SERIAL, meterserial varchar(100), categoryid int not null REFERENCES category(id), category VARCHAR(30) not null REFERENCES category(name), description VARCHAR(80), unit VARCHAR(10), fractionalDigits INT
+    id SERIAL,
+    meterserial varchar(100),
+    categoryid int not null REFERENCES category(id),
+    category VARCHAR(30) not null REFERENCES category(name),
+    description VARCHAR(80),
+    unit VARCHAR(10),
+    fractionalDigits INT
 );
 
 COMMENT ON COLUMN meter.unit IS 'physical unit like kWh or m3';
 
 COMMENT ON COLUMN meter.fractionaldigits IS 'display precission';
 
-create table values (
-    value decimal not null,
-    meter int not NULL references meter(id),
-    date timestamp not null
-);
+CREATE TABLE
+values
+    (
+        value decimal not null,
+        meter int not NULL REFERENCES meter(id),
+        date timestamp not null
+    );
