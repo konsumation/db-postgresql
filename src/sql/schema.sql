fd2519f0a1558f5058450c5d277a8d37c04e8b18
k0nsti <kkarpman@clickforit.de>
2024-03-15 08:34:21 +0100

    refactor: style with prettier sql for vscode

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,13 +1,24 @@
-create table category (
-    id SERIAL, name VARCHAR(30) not null, description VARCHAR(80)
-);
+create table
+    category (
+        id SERIAL,
+        name VARCHAR(30) not null,
+        description VARCHAR(80)
+    );
 
-ALTER TABLE category
-ADD CONSTRAINT category_pk PRIMARY KEY (id, name);
+ALTER TABLE category ADD CONSTRAINT category_pk PRIMARY KEY (id, name);
+
 -- FOREIGN key on category name and id columns instead of two different
-CREATE TABLE meter (
-    id SERIAL PRIMARY KEY, meterserial varchar(100), categoryid int not null, category VARCHAR(30) not null, description VARCHAR(80), unit VARCHAR(10), fractionalDigits INT, Foreign Key (categoryid, category) REFERENCES category (id, name)
-);
+CREATE TABLE
+    meter (
+        id SERIAL PRIMARY KEY,
+        meterserial varchar(100),
+        categoryid int not null,
+        category VARCHAR(30) not null,
+        description VARCHAR(80),
+        unit VARCHAR(10),
+        fractionalDigits INT,
+        Foreign Key (categoryid, category) REFERENCES category (id, name)
+    );
 
 COMMENT ON COLUMN meter.unit IS 'physical unit like kWh or m3';
 
@@ -17,6 +28,6 @@ CREATE TABLE
 values
     (
         value decimal not null,
-        meter int not NULL REFERENCES meter(id),
+        meter int not NULL REFERENCES meter (id),
         date timestamp not null
     );
