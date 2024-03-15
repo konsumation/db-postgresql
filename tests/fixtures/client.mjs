a61f8d857759fff54e9d584ec21c8f60e2d0997e
k0nsti <kkarpman@clickforit.de>
2024-03-15 10:56:58 +0100

    remove debug

--- tests/fixtures/client.mjs
+++ tests/fixtures/client.mjs
@@ -1,12 +1,12 @@
 import pg from 'pg';
 
-console.log(process.env.PGPASSWORD,process.env.POSTGRES_PASSWORD)
+//console.log(process.env.PGPASSWORD,process.env.POSTGRES_PASSWORD)
 const pgclient = new pg.Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     // use env variable PGPASSWORD
-    password: process.env.PGPASSWORD,
+    //password: process.env.PGPASSWORD,
     database: process.env.POSTGRES_DB
 });
 
