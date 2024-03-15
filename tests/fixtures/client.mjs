b8c8a1798ec16f247f1dfd3f7e07639931e581fe
k0nsti <kkarpman@clickforit.de>
2024-03-15 09:37:50 +0100

    fix: use PGPASSWORD variable for connect

--- tests/fixtures/client.mjs
+++ tests/fixtures/client.mjs
@@ -1,10 +1,12 @@
 import pg from 'pg';
 
+
 const pgclient = new pg.Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
+    // use env variable PGPASSWORD
+    //password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB
 });
 
