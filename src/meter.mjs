9ce6822020775686bb1f9d0f673c6f09e3f80dca
Markus Felten <Markus.Felten@gmx.de>
2024-03-27 11:12:29 +0100

    fix: dummy Meter.write()

--- src/meter.mjs
+++ src/meter.mjs
@@ -1,3 +1,19 @@
 import { Meter } from "@konsumation/model";
 
-export class PostgresMeter extends Meter {}
+export class PostgresMeter extends Meter {
+  async write(sql) {
+    if (this.id) {
+      await sql`UPDATE mater SET $${sql(
+        this.attributeValues,
+        ...this.attributeNames
+      )} WHERE id=${this.id}`;
+    } else {
+      this.id = (
+        await sql`INSERT INTO meter ${sql(
+          this.attributeValues,
+          ...this.attributeNames
+        )} RETURNING id`
+      )[0].id;
+    }
+  }
+}
