b315ffcfe4e802aa399df04b8c1d51bac264b77d
Markus Felten <Markus.Felten@gmx.de>
2024-03-27 13:36:49 +0100

    fix: only use present attributes

--- src/note.mjs
+++ src/note.mjs
@@ -2,17 +2,14 @@ import { Note } from "@konsumation/model";
 
 export class PostgresNote extends Note {
   async write(sql) {
+    const values = this.attributeValues;
+    const names = Object.keys(values);
+
     if (this.id) {
-      await sql`UPDATE note SET $${sql(
-        this.attributeValues,
-        ...this.attributeNames
-      )} WHERE id=${this.id}`;
+      await sql`UPDATE note SET ${sql(values, ...names)} WHERE id=${this.id}`;
     } else {
       this.id = (
-        await sql`INSERT INTO note ${sql(
-          this.attributeValues,
-          ...this.attributeNames
-        )} RETURNING id`
+        await sql`INSERT INTO note ${sql(values, ...names)} RETURNING id`
       )[0].id;
     }
   }
