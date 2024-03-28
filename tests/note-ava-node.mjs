3a54e58c993249dafbc8fb93e6163b962d3e04c8
Markus Felten <Markus.Felten@gmx.de>
2024-03-28 11:38:30 +0100

    test: cleanup

--- tests/note-ava-node.mjs
+++ tests/note-ava-node.mjs
@@ -2,4 +2,4 @@ import test from "ava";
 import { Note } from "@konsumation/konsum-db-postgresql";
 import { testNoteConstructor } from "@konsumation/db-test";
 
-test("Note constructor", t => testNoteConstructor(t, Note, { /*categoryid: 1*/ }));
+test("Note constructor", t => testNoteConstructor(t, Note));
