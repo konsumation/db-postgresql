35d39e8aa77698738e2d270865e60dbbc1176a43
Markus Felten <Markus.Felten@gmx.de>
2024-04-06 14:20:55 +0200

    fix: searchParams without optional access

--- src/util.mjs
+++ src/util.mjs
@@ -75,14 +75,13 @@ export function getSchema(url, schema) {
   if (!schema) {
     const u = new URL(url);
     if (u.searchParams.has("currentSchema")) {
-      schema = u.searchParams.get("currentSchema")
-    } else
-      if (u.searchParams?.has("options")) {
-        const m = u.searchParams.get("options")?.match(/search_path=(\w+)/);
-        if (m) {
-          schema = m[1];
-        }
+      schema = u.searchParams.get("currentSchema");
+    } else if (u.searchParams.has("options")) {
+      const m = u.searchParams.get("options")?.match(/search_path=(\w+)/);
+      if (m) {
+        schema = m[1];
       }
+    }
   }
   return schema;
 }
@@ -97,6 +96,8 @@ export function setSchema(url, schema) {
   //do we need any other options on url, if yes replace need to be modified like on getSchema
   url = url.replace(/\?.*$/, "");
   if (schema) {
-    return `${url}?currentSchema=${schema}`
-  } else { return url }
+    return `${url}?currentSchema=${schema}`;
+  } else {
+    return url;
+  }
 }
