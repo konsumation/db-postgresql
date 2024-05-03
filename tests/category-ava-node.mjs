345884ec5e4bbe8c7cb4d8f574b2d4d13b0f83c2
Markus Felten <Markus.Felten@gmx.de>
2024-05-03 12:46:01 +0100

    test: make use of testWriteReadUpdateDeleteCategories()

--- tests/category-ava-node.mjs
+++ tests/category-ava-node.mjs
@@ -14,56 +14,10 @@ test.before(async t => createSchema(process.env.POSTGRES_URL, SCHEMA));
 test.after.always(async t => dropSchema(process.env.POSTGRES_URL, SCHEMA));
 
 test("Category constructor", t =>
-  testCategoryConstructor(t, PostgresCategory, { fractional_digits: 2 })); // TODO defaults
+  testCategoryConstructor(t, PostgresCategory, { fractionalDigits: 2 }));
 
 test("Category write / read / update / delete", async t =>
   testWriteReadUpdateDeleteCategories(
     t,
-    await PostgresMaster.initialize(
-      setSchema(process.env.POSTGRES_URL, SCHEMA)
-    )));
-
-
-test.skip("xCategory write / read / update / delete", async t => {
-  const master = await PostgresMaster.initialize(
-    setSchema(process.env.POSTGRES_URL, SCHEMA)
-  );
-
-  const categories = await testCreateCategories(
-    t,
-    master,
-    Array.from({ length: 10 }, (_, i) => `CAT-${i}`),
-    {},
-    (t, category) => t.true(category.id > 0)
-  );
-
-  const cs = [];
-
-  for await (const c of master.categories(master.context)) {
-    cs.push(c);
-  }
-
-  t.true(cs.length >= categories.length);
-
-  let c = new PostgresCategory({
-    name: `CAT-Update`,
-    description: `Category CAT-insert`
-  });
-  await c.write(master.context);
-  t.is(c.description, `Category CAT-insert`);
-  c.description = "update";
-  c.name = "bla";
-  await c.write(master.context);
-
-  c = await PostgresCategory.entry(master.context, "bla");
-
-  t.is(c.description, `update`);
-  t.is(c.name, `bla`);
-
-  c = await PostgresCategory.entry(master.context, "CAT-12");
-  t.is(c.name, `CAT-12`);
-  t.is(c.description, undefined);
-  await c.delete(master.context);
-
-  await master.close();
-});
+    await PostgresMaster.initialize(setSchema(process.env.POSTGRES_URL, SCHEMA))
+  ));
